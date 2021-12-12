260744d3d1995c46fb1a4a1fdac088696eb26402
Yuragranovsky <yuragranovsky@gmail.com>
2021-12-12 16:47:36 +0200

    hw #19 tests for classes String and StringBuilder

--- src/strings/TestsClassString.java
+++ src/strings/TestsClassString.java
@@ -2,10 +2,14 @@ package strings;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.IllegalFormatException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestsClassString {
+
+	final static String str = "Hello, hello, World!";
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -13,97 +17,158 @@ class TestsClassString {
 
 	@Test
 	void testCharAt() {
-		//TODO
+		assertEquals('e', str.charAt(1));
+		assertEquals(' ', str.charAt(6));
+		try {
+			char t = str.charAt(-1);
+			fail("There should be Exeption");
+		} catch (StringIndexOutOfBoundsException e) {
+			
+		}
 	}
 
 	@Test
 	void testEqualsIgnoreCase() {
-		//TODO
+		assertTrue("hello, hello, woRld!".equalsIgnoreCase(str));
 	}
 
 	@Test
 	void testCompareToIgnoreCase() {
-		//TODO
+		assertTrue(0 > "helllo, WorlD!".compareToIgnoreCase(str));
+		assertTrue(0 < "Hi, hello, World!".compareToIgnoreCase(str));
+		assertTrue(0 == "hello, Hello, world!".compareToIgnoreCase(str));
 	}
 
 	@Test
 	void testStartsWithStringInt() {
-		//TODO
+		assertTrue(str.startsWith("llo", 2));
+		assertFalse(str.startsWith("llo", 0));
 	}
 
 	@Test
 	void testStartsWithString() {
-		//TODO
+		assertTrue(str.startsWith("Hello"));
+		assertFalse(str.startsWith("llo"));
 	}
 
 	@Test
 	void testEndsWith() {
-		//TODO
+		assertTrue(str.endsWith("d!"));
+		assertFalse(str.endsWith("world"));
 	}
 
 	@Test
 	void testIndexOfInt() {
-		//TODO
+		assertEquals(5, str.indexOf(","));
+		assertEquals(-1, str.indexOf("a"));
 	}
 
 	@Test
 	void testIndexOfIntInt() {
-		//TODO
+		assertEquals(9, str.indexOf("l", 5));
+		assertEquals(-1, str.indexOf("e", 9));
 	}
 
 	@Test
 	void testLastIndexOfIntInt() {
-		//TODO
+		assertEquals(3, str.lastIndexOf("l", 5));
+		assertEquals(-1, str.lastIndexOf("h", 5));
 	}
 
 	@Test
 	void testIndexOfString() {
-		//TODO
+		assertEquals(3, str.indexOf("lo"));
+		assertEquals(-1, str.indexOf("lol"));
 	}
 
 	@Test
 	void testLastIndexOfString() {
-		//TODO
+		assertEquals(10, str.lastIndexOf("lo"));
+		assertEquals(-1, str.lastIndexOf("lol"));
 	}
 
 	@Test
 	void testSubstringInt() {
-		//TODO
+		assertEquals("hello, World!", str.substring(7));
+		try {
+			String t = str.substring(-5);
+			fail("There should be IndexOutOfException");
+		} catch (IndexOutOfBoundsException e) {
+			
+		}
+		try {
+			String t = str.substring(77);
+			fail("There should be IndexOutOfException");
+		} catch (IndexOutOfBoundsException e) {
+			
+		}
 	}
 
 	@Test
 	void testSubstringIntInt() {
-		//TODO
+		assertEquals("hello", str.substring(7, 12));
+		try {
+			String t = str.substring(-5, 3);
+			fail("There should be IndexOutOfException");
+		} catch (IndexOutOfBoundsException e) {
+		
+		}
+		try {
+			String t = str.substring(3, 77);
+			fail("There should be IndexOutOfException");
+		} catch (IndexOutOfBoundsException e) {
+			
+		}	
 	}
 
 	@Test
 	void testContains() {
-		//TODO
+		assertTrue(str.contains("hello"));
+		assertFalse(str.contains("world!"));
 	}
 
 	@Test
 	void testJoinCharSequenceCharSequenceArray() {
-		//TODO
+		assertEquals("hello:hello", String.join(":", "hello", "hello"));
+		try {
+			String t = String.join(null, "hello", null);
+			fail("There should be NullPointerException");
+		} catch (NullPointerException e) {
+			
+		}
 	}
 
 	@Test
 	void testToLowerCase() {
-		//TODO
+		assertEquals("hello, hello, world!", str.toLowerCase());
+		assertFalse("Hello, hello, World!".equals(str.toLowerCase()));
 	}
 
 	@Test
 	void testTrim() {
-		//TODO
+		String actual1 = " Hello, World! ";
+		String actual2 = "Hello, World!";
+		String expected = "Hello, World!";
+		assertEquals(expected, actual1.trim());
+		assertEquals(expected, actual2.trim());
 	}
 
 	@Test
 	void testToCharArray() {
-		//TODO
+		String actual = "Hello!";
+		char[] expected = {'H', 'e', 'l', 'l', 'o', '!'};
+		assertArrayEquals(expected, actual.toCharArray());
 	}
 
 	@Test
 	void testFormatStringObjectArray() {
-		//TODO
+		assertEquals("I said: Hello, hello, World!", String.format("I said: %s", str));
+		try {
+			String t = String.format("I said: %d", str);
+			fail("There should be IllegalFormatException");
+		} catch (IllegalFormatException e) {
+			
+		}
 	}
 
 }
--- src/strings/TestsClassStringBuilder.java
+++ src/strings/TestsClassStringBuilder.java
@@ -2,28 +2,55 @@ package strings;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestsClassStringBuilder {
 
+StringBuilder strBuilder = new StringBuilder("StringBuilder");
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		strBuilder = new StringBuilder("StringBuilder");
+	}
+
 	@Test
 	void testAppendString() {
-		//TODO
+		assertEquals("StringBuilderClass", strBuilder.append("Class").toString());
 	}
 
 	@Test
 	void testReplaceIntIntString() {
-		//TODO
+		assertEquals("StringClass", strBuilder.replace(6,  13, "Class").toString());
+		try {
+			StringBuilder t = strBuilder.replace(20, 25, "Class");
+			fail("There should be StringIndexOutOfBoundsException");
+		} catch (StringIndexOutOfBoundsException e) {
+			
+		}
+		try {
+			StringBuilder t = strBuilder.replace(6, 13, null);
+			fail("There should be NullPointerException");
+		} catch (NullPointerException e) {
+			
+		}
 	}
 
 	@Test
 	void testInsertIntString() {
-		//TODO
+		assertEquals("String Builder", strBuilder.insert(6, " ").toString());
+		try {
+			StringBuilder t = strBuilder.insert(25, "Class");
+			fail("There should be StringIndexOutOfBoundsException");
+		} catch (StringIndexOutOfBoundsException e) {
+			
+		}
 	}
 
 	@Test
 	void testReverse() {
-		//TODO
+		assertEquals("redliuBgnirtS", strBuilder.reverse().toString());
+		
 	}
 
 }
